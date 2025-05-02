9251398fad57483bef66d219e2942f2e740852e9
RittikaDev <rittika.dev@gmail.com>
2025-05-02 12:39:26 +0600

    fix: cookie configuration

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,12 @@ const routes_1 = __importDefault(require("./app/routes"));
 const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000", "https://localhost:3000"],
+    origin: [
+        "http://localhost:3000",
+        "https://localhost:3000",
+        "https://127.0.0.1:3000",
+        "http://127.0.0.1:3000",
+    ],
     credentials: true,
 }));
 app.use((0, cookie_parser_1.default)());
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 const app: Application = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://localhost:3000",
+      "https://127.0.0.1:3000",
+      "http://127.0.0.1:3000",
+    ],
     credentials: true,
   })
 );
